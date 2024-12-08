210c4978b5fa49bf03a7ec55436c9b147b9e7852
MD. Akil Mahmud <mdakilmahmud.ak@gmail.com>
2024-12-08 09:08:24 +0600

    Add files via upload

---
+++ exercise 2.py
@@ -0,0 +1,4 @@
+my_list=["Hello","winter","Cold","Fever"]
+for i in my_list:
+  if len(i)>4:
+    print(i, end=" ")
---
+++ exercise1.py
@@ -0,0 +1,3 @@
+my_list=["Hello","winter","Cold","Fever"]
+for i in my_list:
+  print(i, end=" ")
